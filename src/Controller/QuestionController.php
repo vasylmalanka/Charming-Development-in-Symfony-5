1bb0301e4ff91c48175be246733298183ddc61e5
Vasyl Malanka <vsl.malanka@gmail.com>
2020-11-23 15:00:43 +0100

    Move question route configuration to annotation

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage(): Response
     {
         return new Response('What a bewitching controller we have conjured!');
