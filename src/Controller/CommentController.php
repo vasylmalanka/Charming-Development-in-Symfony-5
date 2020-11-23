b9366eed011fe0806e1112e4331bc93a94c0c7ad
Vasyl Malanka <vsl.malanka@gmail.com>
2020-11-23 16:47:32 +0100

    Add comment controller with commentVote action

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    /**
+     * @Route("/comments/{id}/vote/{direction}")
+     */
+    public function commentVote(int $id, string $direction): Response
+    {
+        // todo us id to query database
+
+        if ($direction === 'up') {
+            $currentVoteCount = random_int(7, 100);
+        } else {
+            $currentVoteCount = random_int(0, 5);
+        }
+
+        return $this->json(['votes' => $currentVoteCount]);
+    }
+}
