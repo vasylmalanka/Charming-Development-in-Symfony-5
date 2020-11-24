907f8daab3574040ab265497794994194e3f9fd6
Vasyl Malanka <vsl.malanka@gmail.com>
2020-11-24 10:19:59 +0100

    Use Twig serivce directly for homepage rendering

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,15 +6,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(): Response
+    public function homepage(Environment $twigEnvironment): Response
     {
-        return $this->render('question/homepage.html.twig');
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+
+        return new Response($html);
+//        return $this->render('question/homepage.html.twig');
     }
 
     /**
