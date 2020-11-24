231b26d5be24a97e7d9d9a4b5fb2d1d9450ab0d4
Vasyl Malanka <vsl.malanka@gmail.com>
2020-11-24 10:17:22 +0100

    Add log info to vote events

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +13,15 @@ class CommentController extends AbstractController
     /**
      * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote(int $id, string $direction): Response
+    public function commentVote(int $id, string $direction, LoggerInterface $logger): Response
     {
         // todo us id to query database
 
         if ($direction === 'up') {
+            $logger->info('Voting up!');
             $currentVoteCount = random_int(7, 100);
         } else {
+            $logger->info('Voting down!');
             $currentVoteCount = random_int(0, 5);
         }
 
